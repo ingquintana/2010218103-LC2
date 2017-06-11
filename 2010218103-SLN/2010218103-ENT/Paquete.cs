b21e101d406c0dd5466de471d564c14af559469a
Kevin Quintana <kevinquintana256@gmail.com>
2017-06-10 23:11:27 -0500

    LC3-LC4 versión 2

---
+++ 2010218103-SLN/2010218103-ENT/Paquete.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2010218103_ENT
+{
+    public class Paquete
+    {
+        public int PaqueteId { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Lugar { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public DateTime Fecha { get; set; }
+    }
+}
---
+++ 2010218103-SLN/2010218103-PER/PaqueteTurísticoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using _2010218103_ENT;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2010218103_PER
+{
+    public class PaqueteTurísticoDbContext : DbContext
+    {
+        public DbSet<Paquete> Paquetes { get; set; }
+
+        public PaqueteTurísticoDbContext():base ("PaqueteTurístico")
+        {
+
+        }
+    }
+}
